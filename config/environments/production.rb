ed86e3414a565897ea6892ce7b1a006bb3502727
na-toyoshima <na.toyoshim@gmail.com>
2020-06-09 08:55:52 +0000

    deploy setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-config.action_cable.allowed_request_origins = [ 'http://54.238.86.12/' ]
+config.action_cable.allowed_request_origins = ['http://yurusuta.work/', /http:\/\/yurusuta.*/ ]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
