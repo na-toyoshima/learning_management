dbdb6524eb5cb47722429fd1fb3c61d873f9d878
na-toyoshima <na.toyoshim@gmail.com>
2020-06-11 04:46:27 +0000

    [fix]routing
    -mrootパス変更

--- app/controllers/parent/sessions_controller.rb
+++ app/controllers/parent/sessions_controller.rb
@@ -31,6 +31,6 @@ class Parent::SessionsController < Devise::SessionsController
   end
 
   def after_sign_out_path_for(resource)
-    student_root_path
+    root_path
   end
 end
--- app/controllers/student/sessions_controller.rb
+++ app/controllers/student/sessions_controller.rb
@@ -31,6 +31,6 @@ class Student::SessionsController < Devise::SessionsController
   end
 
   def after_sign_out_path_for(resource)
-    student_root_path
+    root_path
   end
 end
